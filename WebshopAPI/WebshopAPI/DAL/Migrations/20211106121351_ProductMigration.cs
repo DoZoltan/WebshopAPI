58ebf3c08faaf32376e620a08fb94c9e1bd43404
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-06 13:52:58 +0100

    First migration

---
+++ WebshopAPI/WebshopAPI/DAL/Migrations/20211106121351_ProductMigration.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebshopAPI.DAL.Migrations
+{
+    public partial class ProductMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CPUs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CoreNumber = table.Column<int>(type: "int", nullable: false),
+                    L3Cache = table.Column<int>(type: "int", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    SocketType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcquisitionPrice = table.Column<int>(type: "int", nullable: false),
+                    SellPrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CPUs", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Motherboards",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Usb3Amount = table.Column<int>(type: "int", nullable: false),
+                    Wifi = table.Column<bool>(type: "bit", nullable: false),
+                    SizeStandard = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPUSocketType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MemorySocketType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxMemorySize = table.Column<int>(type: "int", nullable: false),
+                    NumberOfMemorySockets = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcquisitionPrice = table.Column<int>(type: "int", nullable: false),
+                    SellPrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motherboards", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RAMs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Gb = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Delay = table.Column<int>(type: "int", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    SocketType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcquisitionPrice = table.Column<int>(type: "int", nullable: false),
+                    SellPrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RAMs", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CPUs");
+
+            migrationBuilder.DropTable(
+                name: "Motherboards");
+
+            migrationBuilder.DropTable(
+                name: "RAMs");
+        }
+    }
+}

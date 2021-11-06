5bc13d73780a4f3ac38b1a3e1c99258aee802e38
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-06 16:25:37 +0100

    Generic DAL Class

---
+++ WebshopAPI/WebshopAPI/DAL/DALClasses/BaseDAL.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebshopAPI.DAL.DALInterfaces;
+using WebshopAPI.DAL.Models;
+
+namespace WebshopAPI.DAL.DALClasses
+{
+    // Meg kell mondani, h a T milyen típusú lehet (jelenleg BaseProduct) -->
+    // innen tudni, h annak milyen property-je / metódusai vannak
+    public class BaseDAL<T> : IBaseDAL<T> where T : BaseProduct
+    {
+        private readonly ShopContext _context;
+
+        public BaseDAL(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddNew(T product)
+        {
+            try
+            {
+                // A T típusátol függően legyen a megfelelő DbSet<> kiválasztva a context osztályban
+                // majd azon legyen végrehalytva a művelet
+                _context.Set<T>().Add(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return null;
+            }
+
+            var lastId = await _context.Set<T>().MaxAsync(prod => prod.ID);
+            return await GetByID(lastId);
+        }
+
+        public async Task<T> DeleteByID(int id)
+        {
+            var productForDeleting = await _context.Set<T>().FirstOrDefaultAsync(prod => prod.ID == id);
+
+            if (productForDeleting != null)
+            {
+                _context.Set<T>().Remove(productForDeleting);
+                await _context.SaveChangesAsync();
+            }
+            return productForDeleting;
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByID(int id)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(prod => prod.ID == id);
+        }
+
+        public async Task<T> Update(T product)
+        {
+            try
+            {
+                _context.Set<T>().Update(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return null;
+            }
+
+            return await GetByID(product.ID);
+        }
+    }
+}

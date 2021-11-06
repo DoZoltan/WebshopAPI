91e11492f98be6bacd6b73e5c375fa2788562dbc
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-06 14:08:52 +0100

    New migration

--- WebshopAPI/WebshopAPI/DAL/Migrations/20211106130725_ProductMigrationWithSeed.Designer.cs
+++ WebshopAPI/WebshopAPI/DAL/Migrations/20211106130725_ProductMigrationWithSeed.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebshopAPI.DAL;
 
 namespace WebshopAPI.DAL.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20211106130725_ProductMigrationWithSeed")]
+    partial class ProductMigrationWithSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

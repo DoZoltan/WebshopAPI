b05b87ede40318f3696c71b93cd7e372a094d8ed
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-07 13:05:56 +0100

    Implemented BaseBLL

--- WebshopAPI/WebshopAPI/BLL/Classes/BaseBLL.cs
+++ WebshopAPI/WebshopAPI/BLL/Classes/BaseBLL.cs
@@ -3,35 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebshopAPI.BLL.Interfaces;
+using WebshopAPI.DAL.DALInterfaces;
 using WebshopAPI.DAL.Models;
 
 namespace WebshopAPI.BLL.Classes
 {
     public class BaseBLL<T> : IBaseBLL<T> where T : BaseProduct
     {
-        public Task<T> AddNew(T product)
+        protected readonly IBaseDAL<T> _baseDAL;
+
+        public BaseBLL(IBaseDAL<T> baseDAL)
         {
-            throw new NotImplementedException();
+            _baseDAL = baseDAL;
         }
 
-        public Task<T> DeleteByID(int id)
+        public async Task<T> AddNew(T product)
         {
-            throw new NotImplementedException();
+            if (product != null)
+            {
+                return await _baseDAL.AddNew(product);
+            }
+
+            return null;
         }
 
-        public Task<IEnumerable<T>> GetAll()
+        public async Task<T> DeleteByID(int id)
         {
-            throw new NotImplementedException();
+            var product = await GetByID(id);
+
+            if (product != null)
+            {
+                return await _baseDAL.Delete(product);
+            }
+
+            return null;
         }
 
-        public Task<T> GetByID(int id)
+        public async Task<IEnumerable<T>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _baseDAL.GetAll();
         }
 
-        public Task<T> Update(T product)
+        public async Task<T> GetByID(int id)
         {
-            throw new NotImplementedException();
+            if (id > 0)
+            {
+                return await _baseDAL.GetByID(id);
+            }
+
+            return null;
+        }
+
+        public async Task<T> Update(T product)
+        {
+            if (product != null)
+            {
+                return await _baseDAL.Update(product);
+            }
+
+            return null;
         }
     }
 }

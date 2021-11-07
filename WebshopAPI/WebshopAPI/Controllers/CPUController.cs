aa0b9fb5e7cfb56517df08f51685b250cf9c84b0
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-07 15:22:36 +0100

    CPU controller

--- WebshopAPI/WebshopAPI/Controllers/CPUController.cs
+++ WebshopAPI/WebshopAPI/Controllers/CPUController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebshopAPI.BLL.Interfaces;
+using WebshopAPI.DAL.Models;
+using WebshopAPI.Enums;
 
 namespace WebshopAPI.Controllers
 {
@@ -11,5 +14,89 @@ namespace WebshopAPI.Controllers
     [ApiController]
     public class CPUController : ControllerBase
     {
+        protected readonly ICPUBLL _CPUBLL;
+
+        public CPUController(ICPUBLL CPUBLL)
+        {
+            _CPUBLL = CPUBLL;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _CPUBLL.GetByID(id);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _CPUBLL.GetAll();
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CPU cpu)
+        {
+            var result = await _CPUBLL.AddNew(cpu);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Add new CPU was failed");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] CPU cpu)
+        {
+            var result = await _CPUBLL.Update(cpu);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Updating the CPU was failed");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _CPUBLL.DeleteByID(id);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Deleting the CPU was failed");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCPUsByMotherboard(CPUSocketEnum CPUSocket)
+        {
+            var result = await _CPUBLL.GetCompatibleCPUsByMotherboardCPUSocket(CPUSocket);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
+        }
     }
 }

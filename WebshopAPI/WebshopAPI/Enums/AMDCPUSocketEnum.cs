96a26eca4dee76fc1e012991d26d1121acff99fb
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-07 10:38:32 +0100

    CPU socket enums

---
+++ WebshopAPI/WebshopAPI/Enums/AMDCPUSocketEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebshopAPI.Enums
+{
+    public enum AMDCPUSocketEnum
+    {
+        AM2 = 1,
+        AM2Plus = 2,
+        AM3 = 3,
+        G34 = 4,
+        C32 = 5,
+        FM1 = 6,
+        FS1 = 7,
+        AM3Plus = 8,
+        FM2 = 9,
+        AM1 = 10,
+        AM4 = 11,
+        SP3 = 12,
+        TR4 = 13,
+        sTRX4 = 14,
+    }
+}
---
+++ WebshopAPI/WebshopAPI/Enums/IntelCPUSocketEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebshopAPI.Enums
+{
+    public enum IntelCPUSocketEnum
+    {
+        LGA1366 = 1,
+        LGA1156 = 2,
+        LGA1567 = 3,
+        LGA1155 = 4,
+        LGA2011 = 5,
+        LGA1356 = 6,
+        LGA1150 = 7,
+        LGA2011v3 = 8,
+        LGA1151 = 9,
+        LGA3647 = 10,
+        LGA2066 = 11,
+        LGA1200 = 12,
+        LGA1700 = 13,
+    }
+}

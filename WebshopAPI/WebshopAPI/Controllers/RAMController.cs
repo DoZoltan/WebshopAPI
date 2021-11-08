1c5978796593f8b46bd6e1cc50747c4a10dd6e81
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-08 16:08:06 +0100

    RAM controller

--- WebshopAPI/WebshopAPI/Controllers/RAMController.cs
+++ WebshopAPI/WebshopAPI/Controllers/RAMController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebshopAPI.BLL.Interfaces;
+using WebshopAPI.DAL.Models;
+using WebshopAPI.Enums;
 
 namespace WebshopAPI.Controllers
 {
@@ -17,6 +19,84 @@ namespace WebshopAPI.Controllers
         public RAMController(IRAMBLL RAMBLL)
         {
             _RAMBLL = RAMBLL;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _RAMBLL.GetByID(id);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _RAMBLL.GetAll();
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] RAM ram)
+        {
+            var result = await _RAMBLL.AddNew(ram);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Add new ram was failed");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] RAM ram)
+        {
+            var result = await _RAMBLL.Update(ram);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Updating the ram was failed");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _RAMBLL.DeleteByID(id);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Deleting the ram was failed");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMemoriesByMotherboard(RAMSocketTypeEnum memorySocket)
+        {
+            var result = await _RAMBLL.GetCompatibleMemoriesByMotherboardMemorySocket(memorySocket);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound("No result");
         }
     }
 }

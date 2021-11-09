372854dc6b3edc3fe244a94100b4bbd46381ef28
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-09 09:17:03 +0100

    Custom model validators

---
+++ WebshopAPI/WebshopAPI/Services/ModelValidators/CpuSocketValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebshopAPI.DAL.Models;
+using WebshopAPI.Enums;
+
+namespace WebshopAPI.Services.ModelValidators
+{
+    public class CpuSocketValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var obj = validationContext.ObjectType;
+
+            if (obj != null && Enum.IsDefined(typeof(CpuSocketEnum), value))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("CPU socket type is invalid");
+        }
+    }
+}
---
+++ WebshopAPI/WebshopAPI/Services/ModelValidators/RamSocketValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebshopAPI.DAL.Models;
+using WebshopAPI.Enums;
+
+namespace WebshopAPI.Services.ModelValidators
+{
+    public class RamSocketValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var obj = validationContext.ObjectType;
+
+            if (obj != null && Enum.IsDefined(typeof(RamSocketTypeEnum), value))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("RAM socket type is invalid");
+        }
+    }
+}

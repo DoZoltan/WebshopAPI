f6ba7894c6b019f6449d27392e6675d90105ad87
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-12-03 10:47:38 +0100

    Inherit the generic controller

--- WebshopAPI/WebshopAPI/Controllers/CPUController.cs
+++ WebshopAPI/WebshopAPI/Controllers/CPUController.cs
@@ -10,83 +10,13 @@ namespace WebshopAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CpuController : ControllerBase
+    public class CpuController : BaseCRUDController<Cpu>
     {
         protected readonly ICpuBLL _CpuBLL;
-        protected readonly IResponseMessenger _ResponseMessenger;
 
-        public CpuController(ICpuBLL CpuBLL, IResponseMessenger ResponseMessenger)
+        public CpuController(ICpuBLL CpuBLL, IResponseMessenger ResponseMessenger): base(CpuBLL, ResponseMessenger)
         {
             _CpuBLL = CpuBLL;
-            _ResponseMessenger = ResponseMessenger;
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            var result = await _CpuBLL.GetByID(id);
-
-            if (result != null)
-            {
-                return Ok(result);
-            }
-
-            return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Cpu).Name, id));
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            return Ok(await _CpuBLL.GetAll());
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add([FromBody] Cpu cpu)
-        {
-            var result = await _CpuBLL.AddNew(cpu);
-
-            if (result != null)
-            {
-                return CreatedAtAction(nameof(Get), new { id = result.ID }, result);
-            }
-
-            return UnprocessableEntity(_ResponseMessenger.SendWrongProductDataMessage());
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update([FromBody] Cpu cpu, int id)
-        {
-            if (cpu.ID != id)
-            {
-                return BadRequest(_ResponseMessenger.SendWrongProductIdMessage(typeof(Cpu).Name, cpu.ID, id));
-            }
-
-            if (await _CpuBLL.GetByID(cpu.ID) == null)
-            {
-                return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Cpu).Name, id));
-            }
-
-            var result = await _CpuBLL.Update(cpu);
-
-            if (result != null)
-            {
-                return Ok(result);
-            }
-
-            return UnprocessableEntity(_ResponseMessenger.SendWrongProductDataMessage());
-        }
-
-        [HttpDelete("delete/{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var ramToDelete = await _CpuBLL.GetByID(id);
-
-            if (ramToDelete == null)
-            {
-                return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Cpu).Name, id));
-            }
-
-            return Ok(await _CpuBLL.Delete(ramToDelete));
         }
 
         [HttpGet("socket/{socket}")]
--- WebshopAPI/WebshopAPI/Controllers/MotherboardController.cs
+++ WebshopAPI/WebshopAPI/Controllers/MotherboardController.cs
@@ -10,83 +10,13 @@ namespace WebshopAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MotherboardController : ControllerBase
+    public class MotherboardController : BaseCRUDController<Motherboard>
     {
         protected readonly IMotherboardBLL _motherboardBLL;
-        protected readonly IResponseMessenger _ResponseMessenger;
 
-        public MotherboardController(IMotherboardBLL motherboardBLL, IResponseMessenger ResponseMessenger)
+        public MotherboardController(IMotherboardBLL motherboardBLL, IResponseMessenger ResponseMessenger): base(motherboardBLL, ResponseMessenger)
         {
             _motherboardBLL = motherboardBLL;
-            _ResponseMessenger = ResponseMessenger;
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            var result = await _motherboardBLL.GetByID(id);
-
-            if (result != null)
-            {
-                return Ok(result);
-            }
-
-            return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Motherboard).Name, id));
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            return Ok(await _motherboardBLL.GetAll());
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add([FromBody] Motherboard motherboard)
-        {
-            var result = await _motherboardBLL.AddNew(motherboard);
-
-            if (result != null)
-            {
-                return CreatedAtAction(nameof(Get), new { id = result.ID }, result);
-            }
-
-            return UnprocessableEntity(_ResponseMessenger.SendWrongProductDataMessage());
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update([FromBody] Motherboard motherboard, int id)
-        {
-            if (motherboard.ID != id)
-            {
-                return BadRequest(_ResponseMessenger.SendWrongProductIdMessage(typeof(Motherboard).Name, motherboard.ID, id));
-            }
-
-            if (await _motherboardBLL.GetByID(motherboard.ID) == null)
-            {
-                return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Motherboard).Name, id));
-            }
-
-            var result = await _motherboardBLL.Update(motherboard);
-
-            if (result != null)
-            {
-                return Ok(result);
-            }
-
-            return UnprocessableEntity(_ResponseMessenger.SendWrongProductDataMessage());
-        }
-
-        [HttpDelete("delete/{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var ramToDelete = await _motherboardBLL.GetByID(id);
-
-            if (ramToDelete == null)
-            {
-                return NotFound(_ResponseMessenger.SendProductIdNotFoundMessage(typeof(Motherboard).Name, id));
-            }
-
-            return Ok(await _motherboardBLL.Delete(ramToDelete));
         }
 
         [HttpGet("cpuSocket/{cpuSocket}")]

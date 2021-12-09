ee58f0627e71142ecb3f33386dc747f17c3006b1
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-12-09 08:47:31 +0100

    DTO converter service

---
+++ WebshopAPI/WebshopAPI/Services/DTOConverter/DTOConverter.cs
@@ -0,0 +1,19 @@
+﻿using WebshopAPI.DAL.DTOs;
+using WebshopAPI.DAL.Models;
+
+namespace WebshopAPI.Services.DTOConverter
+{
+    public static class DTOConverter
+    {
+        public static ProductGridDataDTO AsProductGridDataDTO(this BaseProduct baseProduct)
+        {
+            return new ProductGridDataDTO
+            {
+                ID = baseProduct.ID,
+                ProductName = baseProduct.ProductName,
+                Brand = baseProduct.Brand,
+                SellPrice = baseProduct.SellPrice
+            };
+        }
+    }
+}

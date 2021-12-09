72959a8375da85543bee7224118c92d994a48a00
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-12-09 08:47:08 +0100

    DTO to show only the needed data at the product grid

---
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ProductGridDataDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebshopAPI.DAL.DTOs
+{
+    public class ProductGridDataDTO
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        public int SellPrice { get; set; }
+    }
+}

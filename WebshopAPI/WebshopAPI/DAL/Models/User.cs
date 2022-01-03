a1362ace853d8957356d66b86e875f754110f759
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-03 11:57:46 +0100

    User model

---
+++ WebshopAPI/WebshopAPI/DAL/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace WebshopAPI.DAL.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public ICollection<BaseProduct> ProductsInCart { get; set; }
+        public ICollection<BaseProduct> PreviouslyOrderedProducts { get; set; }
+    }
+}

7b7a64ad8cbf77c12548862ea8a2d9837f7107ef
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-04 11:58:29 +0100

    Implemented constructors

--- WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/AuthenticationResponseDTO.cs
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/AuthenticationResponseDTO.cs
@@ -5,6 +5,13 @@ namespace WebshopAPI.DAL.DTOs.ResponseDTOs
 {
     public class AuthenticationResponseDTO
     {
+        public AuthenticationResponseDTO(bool succeeded, List<string> messages, User user)
+        {
+            Succeeded = succeeded;
+            Messages = messages;
+            User = user;
+        }
+
         public bool Succeeded { get; set; }
         public List<string> Messages { get; set; }
         public User User { get; set; }
--- WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/UserDataChangeResponseDTO.cs
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/UserDataChangeResponseDTO.cs
@@ -4,6 +4,12 @@ namespace WebshopAPI.DAL.DTOs.ResponseDTOs
 {
     public class UserDataChangeResponseDTO
     {
+        public UserDataChangeResponseDTO(bool succeeded, List<string> messages)
+        {
+            Succeeded = succeeded;
+            Messages = messages;
+        }
+
         public bool Succeeded { get; set; }
         public List<string> Messages { get; set; }
     }

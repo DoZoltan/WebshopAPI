b36f4ed7bfd547485e7806ff95881f515fc9f1d1
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-05 14:38:52 +0100

    New request and response DTOs

---
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/GetUsersByRoleResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using WebshopAPI.DAL.Models;
+
+namespace WebshopAPI.DAL.DTOs.ResponseDTOs
+{
+    public class GetUsersByRoleResponseDTO
+    {
+        public GetUsersByRoleResponseDTO(bool succeeded, IEnumerable<User> foundUsers, string responseMessage)
+        {
+            Succeeded = succeeded;
+            FoundUsers = foundUsers;
+            ResponseMessage = responseMessage;
+        }
+
+        public bool Succeeded { get; set; }
+        public IEnumerable<User> FoundUsers { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}
---
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/ModifyUserRolesResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace WebshopAPI.DAL.DTOs.ResponseDTOs
+{
+    public class ModifyUserRolesResponseDTO
+    {
+        public ModifyUserRolesResponseDTO(bool succeeded, string responseMessage)
+        {
+            Succeeded = succeeded;
+            ResponseMessage = responseMessage;
+        }
+
+        public bool Succeeded { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}

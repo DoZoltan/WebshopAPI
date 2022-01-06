f4516c34005b778a001f2f42a7e18ef0e800f3e0
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-06 07:50:14 +0100

    DTO rename

--- WebshopAPI/WebshopAPI/DAL/DTOs/RequestDTOs/CreateOrDeleteRoleRequestDTO.cs
+++ WebshopAPI/WebshopAPI/DAL/DTOs/RequestDTOs/CreateOrDeleteRoleRequestDTO.cs
@@ -2,7 +2,7 @@
 
 namespace WebshopAPI.DAL.DTOs.RequestDTOs
 {
-    public class CreateRoleRequestDTO
+    public class CreateOrDeleteRoleRequestDTO
     {
         [Required]
         [RegularExpression(@"^[a-zA-Z]{3,15}$", ErrorMessage = "3-15 characters without any special character")]

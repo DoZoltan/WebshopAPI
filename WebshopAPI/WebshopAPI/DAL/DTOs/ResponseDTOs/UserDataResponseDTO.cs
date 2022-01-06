1fb7447dafe9e29a075603eb0aceac3146d454a7
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-06 10:42:35 +0100

    DTO to send relevant user related data in administration related responses

---
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/UserDataResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebshopAPI.DAL.DTOs.ResponseDTOs
+{
+    public class UserDataResponseDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}

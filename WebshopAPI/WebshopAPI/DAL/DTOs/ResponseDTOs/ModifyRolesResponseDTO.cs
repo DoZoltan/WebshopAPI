e8daa9c9e4c9ce8b2461858a89948922b3838e8e
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-06 08:58:57 +0100

    Removed the single string response and the constructor overload

--- WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/ModifyRolesResponseDTO.cs
+++ WebshopAPI/WebshopAPI/DAL/DTOs/ResponseDTOs/ModifyRolesResponseDTO.cs
@@ -4,12 +4,6 @@ namespace WebshopAPI.DAL.DTOs.ResponseDTOs
 {
     public class ModifyRolesResponseDTO
     {
-        public ModifyRolesResponseDTO(bool succeeded, string responseMessage)
-        {
-            Succeeded = succeeded;
-            ResponseMessage = responseMessage;
-        }
-
         public ModifyRolesResponseDTO(bool succeeded, IEnumerable<string> responseMessages)
         {
             Succeeded = succeeded;
@@ -17,7 +11,6 @@ namespace WebshopAPI.DAL.DTOs.ResponseDTOs
         }
 
         public bool Succeeded { get; set; }
-        public string ResponseMessage { get; set; }
         public IEnumerable<string> ResponseMessages { get; set; }
     }
 }

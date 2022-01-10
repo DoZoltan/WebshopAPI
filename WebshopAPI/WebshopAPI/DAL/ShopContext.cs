ff33edacf8aef80afce652f192d44cf6934e4654
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-10 10:56:07 +0100

    Deleted cart and cart content models and dbsets

--- WebshopAPI/WebshopAPI/DAL/ShopContext.cs
+++ WebshopAPI/WebshopAPI/DAL/ShopContext.cs
@@ -15,23 +15,7 @@ namespace WebshopAPI.DAL
         public DbSet<Cpu> Cpus { get; set; }
         public DbSet<Ram> Rams { get; set; }
         public DbSet<Motherboard> Motherboards { get; set; }
-        public DbSet<ShoppingCart> Carts { get; set; }
         public DbSet<Order> Orders { get; set; }
-        public DbSet<CartContent> CartContent { get; set; }
-        public DbSet<OrderContent> OrderContent { get; set; }
-
-        /*
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<User>()
-            //    .HasOne(u => u.Cart)
-            //    .WithOne(c => c.Owner)
-            //    .HasForeignKey<ShoppingCart>(c => c.OwnerId);
-
-            // A ModelBuilder-hez írt extension metódus meghívása
-            modelBuilder.Seed();
-            
-            base.OnModelCreating(modelBuilder);
-        }*/
+        public DbSet<OrderItems> OrderItems { get; set; }
     }
 }
